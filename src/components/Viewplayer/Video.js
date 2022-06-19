9be0c10874fd0823f2f8c6540cc54350500cd11a
Joe M <mccarthysound@gmail.com>
2022-06-19 18:55:51 -0400

    iOS fixes

--- src/components/Viewplayer/Video.js
+++ src/components/Viewplayer/Video.js
@@ -9,7 +9,8 @@ function Video(props) {
 
     return (
         <video className="media" loop muted autoPlay playsInline ref={videoElem}>
-            <source src={`${process.env.PUBLIC_URL}/video/${props.src}`} type="video/webm"></source>
+            <source src={`${process.env.PUBLIC_URL}/video/${props.src}`} type='video/webm; codecs="vp9"'></source>
+            <source src={`${process.env.PUBLIC_URL}/video/${props.src.replace('VP9', 'VP8')}`} type='video/webm; codecs="vp8"'></source>
         </video>
     );
 }
